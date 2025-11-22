8ca4ee88d24c833732a4c7785c46f32d2f6f9828
Rishi Tak <Takrishi007@gmail.com>
2025-11-22 21:28:51 +0530

    Initial Commit

---
+++ FindMY/FindMYApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMYApp.swift
+//  FindMY
+//
+//  Created by Rishi Tak on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FindMYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindMYTests/FindMYTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindMYTests.swift
+//  FindMYTests
+//
+//  Created by Rishi Tak on 22/11/25.
+//
+
+import Testing
+@testable import FindMY
+
+struct FindMYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
